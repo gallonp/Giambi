51cae583548265b84b8af46b36b2b610f8162793
cwl <cwl@cwl-PC-mac>
2014-01-17 01:58:02 +0900

    test

--- src/mainClass.java
+++ src/mainClass.java
@@ -10,6 +10,14 @@ public class mainClass {
 	    // Haoli second commit
 		// Jian third
 		//Jialiang Fourth Haoli
+		try{
+			int i = 0/0;
+			System.out.println(i);
+		} catch (Exception e){
+			return;
+		} finally {
+			System.out.println("Finally we got the truth.");
+		}
 	}
 
 }
