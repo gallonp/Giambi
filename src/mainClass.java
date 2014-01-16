53890cd895e73c039293a4a087bc80bdedaf5fb7
Haoli Du <henry54809@gmail.com>
2014-01-16 14:54:55 -0500

    test

--- src/mainClass.java
+++ src/mainClass.java
@@ -9,7 +9,22 @@ public class mainClass {
 		//Giambi first commit!!!!!!!!
 	    // Haoli second commit
 		// Jian third
+<<<<<<< HEAD
 		//Jialiang  Haoli
+=======
+		//Jialiang Fourth Haoli
+		//Jian Fifth
+		try{
+			int i = 0/0;
+			System.out.println(i);
+		} catch (Exception e){
+			return;
+		} finally {
+			System.out.println("Finally we got the truth.");
+		}
+		
+		// Jian 12:20 pm
+>>>>>>> 37205dd277257336aee130ffc8c16b3bef9f6866
 	}
 
 }
